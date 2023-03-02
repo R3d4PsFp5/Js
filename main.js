d7083ef6fd1e847f11a5de409f65c1e6af658735
Sergey <sergey3>
2023-03-02 21:18:03 +0500

    v1

---
+++ main.js
@@ -0,0 +1,12 @@
+var x = +prompt('Введите число x');
+var y = +prompt('Введите число y');
+var z = +prompt('Введите число z');
+  
+console.log(x);
+
+console.log(y);
+
+console.log(z);
+
+
+console.log((x+y+z)/3);
